7eb1abae622ad6338f85bf6fcec072629a84618c
mohdaamil120 <mohdaamil120@gmail.com>
2024-11-14 04:23:57 +0530

    FeaturesComponent added

--- src/Components/Features/FeaturesComponent.jsx
+++ src/Components/Features/FeaturesComponent.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import FigmaIcon from "../../SvgIcons/FigmaIcon"
+import TickIcon from '../../SvgIcons/TickIcon'
+import DownloadIcon from '../../SvgIcons/DownloadIcon'
 
 const FeaturesComponent = () => {
   return (
@@ -7,9 +10,59 @@ const FeaturesComponent = () => {
         <div className='main__container'>
             <div className='featured__container'>
                 <div className='description'>
-                    
+                    <h3>Description</h3>
+                    <p>
+                        Hello Friends👋🏻
+                        <br />
+                        <br />
+                        Achieving your goals is now simpler than ever with Beetroot. Take control of your habits by setting personalized goals, receiving timely reminders, and effortlessly tracking your progress. The app records your journey and provides insights to help you build a healthier, more organized life. It addresses several challenges, from maintaining consistency to monitoring your overall progress. Your path to a better you has never been this straightforward!
+                        <br />
+                        <br />
+                        I also want to share my exploration of our app's features. You can set specific quitting goals, find tips for overcoming cravings, and access support resources whenever you need them. I hope you guys find it useful. Feel free to comment!
+                        <br />
+                        <br />
+                        🔥Checkout our other product - https://ui8.net/omega-orion/products/forgo---quit-smoking-app✉️ 
+                    </p>
                 </div>
-                <div className='Features'></div>
+                <div className='features'>
+                    <h3>Features</h3>
+                    <div className='features__options'>
+                        <div>
+                            <TickIcon/>
+                            <p>120+ Screens</p>
+                        </div>
+                        <div>
+                            <TickIcon/>
+                            <p>Fully customisable designs</p>
+                        </div>
+                        <div>
+                            <TickIcon/>
+                            <p>Organised Design system</p>
+                        </div>
+                        <div>
+                            <TickIcon/>
+                            <p>Light & Dark mode</p>
+                        </div>
+                        <div>
+                            <TickIcon/>
+                            <p>Customised Illustration</p>
+                        </div>
+                        <div>
+                            <TickIcon/>
+                            <p>Neatly Organised layer</p>
+                        </div>
+                    </div>
+                    <div className='format'>
+                        <h3>Format</h3>
+                        <div className='format__icons'>
+                            <FigmaIcon/>
+                            <div>
+                                <DownloadIcon/>
+                                <p>15.2mb</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </DIV>
@@ -23,10 +76,94 @@ const DIV=styled.div`
  *{
         font-family: 'Inter Tight', sans-serif;
   }
-    .main__container{
+  .main__container{
         width: 1440px;
         margin: auto;
         background-color: #0C0C0C;
+  }
+
+
+  .featured__container{
+    display: flex;
+    gap: 100px;
+  }
+
+
+  .description{
+    margin-left: 100px;
+    padding-bottom: 76px;    
+    width : 597px;
+  }
+
+  .description h3 {
+    width: 125px;
+    font-size: 24px;
+    color: #FFFFFF;
+    line-height: 28px;
+    font-weight: 600;
+    margin-bottom: 40px;
+  }
+  .description p {
+      white-space: pre-wrap;
+      font-size: 15.9px;
+      line-height: 24px;
+      color: #FFFFFF;
+      font-weight: 400; 
+    }
+
+    .features{
+        display: flex;
+        flex-direction: column;
+        gap: 40px;
+    }
+    .features h3 {
+      width: 95px;
+      font-size: 24px;
+      color: #FFFFFF;
+      line-height: 28px;
+      font-weight: 600;
+    }
+
+    .features__options{
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+    }
+
+    .features__options > div{
+        display:flex;
+        gap: 8px;
+    }
+    .features__options > div p{
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+        letter-spacing: -2%;
+        color: #F2F2F2;
+    }
+
+    .format{
+        display: flex;
+        flex-direction: column;
+        gap: 24px;
+    }
+
+    .format__icons{
+        display: flex;
+        gap: 12px;
+        cursor: pointer;
+    }
+    .format__icons > div{
+        display: flex;
+        gap: 8px;
+        align-items: center;
+       
+    }
+    .format__icons > div >p{
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+        color: #3D3D3D;
     }
 
 `
---
+++ src/SvgIcons/TickIcon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TickIcon = () => {
+  return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7.5 8.96548L10.0253 11.6667L15.8333 5.83337" stroke="#34C759" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M17.5 10C17.5 14.1421 14.1421 17.5 10 17.5C5.85786 17.5 2.5 14.1421 2.5 10C2.5 5.85786 5.85786 2.5 10 2.5C10.8185 2.5 11.6064 2.63111 12.3438 2.87348" stroke="#34C759" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+
+  )
+}
+
+export default TickIcon
